9dfb4c5acb422cfb8622fcb0a4c8217cbffcee72
Bruno Caregnato <33569494+brunocaregnato@users.noreply.github.com>
2019-06-13 07:15:14 -0300

    Add files via upload

--- TrabalhoBanco2/TrabalhoBanco2/Form1.Designer.cs
+++ TrabalhoBanco2/TrabalhoBanco2/Form1.Designer.cs
@@ -46,8 +46,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(838, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Simulação de um gerente de controle de concorrência utilizando protocolo 2PL Bási" +
-    "co";
+            this.label1.Text = "Simulação de um gerente de controle de concorrência utilizando protocolo 2PL Básico";
             // 
             // groupBox1
             // 
